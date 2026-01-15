c7a08b62aaaf42d0d5d7614d82ab45bbd431cf85
Pranav2000-debug <pranav.ch410@gmail.com>
2026-01-15 20:20:47 +0530

    add sessions setup

---
+++ backend/routes/session.routes.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { protectRoute } from "../middlewares/protectRoute.middleware.js";
+import { createSession, endSession, getActiveSession, getRecentSessions, getSessionById, joinSession } from "../controllers/session.controller.js";
+
+const sessionRouter = express.Router();
+
+sessionRouter.post("/", protectRoute, createSession);
+sessionRouter.get("/active", protectRoute, getActiveSession);
+sessionRouter.get("/my-recent", protectRoute, getRecentSessions);
+
+// session management
+sessionRouter.get("/:id", protectRoute, getSessionById);
+sessionRouter.post("/:id/join", protectRoute, joinSession);
+sessionRouter.get("/:id/end", protectRoute, endSession);
+
+
+export default sessionRouter;
--- backend/services/stream/stream.js
+++ backend/services/stream/stream.js
@@ -1,5 +1,6 @@
 import { StreamChat } from "stream-chat";
 import { ENV } from "../../env.js";
+import { StreamClient } from "@stream-io/node-sdk";
 
 const apiKey = ENV.STREAM_API_KEY;
 const apiSecret = ENV.STREAM_API_SECRET;
@@ -8,8 +9,10 @@ if (!apiKey || !apiSecret) {
   console.error("Stream api key or secret is missing");
 }
 
-export const chatClient = StreamChat.getInstance(apiKey, apiSecret);
+export const chatClient = StreamChat.getInstance(apiKey, apiSecret); // for chat features
+export const streamClient = new StreamClient(apiKey, apiSecret); // for vc
 
+// funcs for chat/user creation in getStream
 export const upsertStreamUser = async (userData) => {
   try {
     await chatClient.upsertUser(userData);
