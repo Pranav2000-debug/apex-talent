6a56c37cc2cc4a303f5bfeabd12cb6f6881b3840
Pranav2000-debug <pranav.ch410@gmail.com>
2026-01-15 19:05:47 +0530

    reviewed imports

--- backend/controllers/chat.controller.js
+++ backend/controllers/chat.controller.js
@@ -5,15 +5,16 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 export const getStreamToken = asyncHandler(async (req, res) => {
   // using clerkId for stream since Stream uses clerkId for a user's id
   // using the create token method, pass the clerkId parameter to generate a "client-side token".
-  const token = chatClient.createToken(req.user?.clerkId);
+  const { clerkId, name, profileImage } = req.user;
+  const token = chatClient.createToken(clerkId);
   return res.status(200).json(
     new ApiResponse(
       200,
       {
         token,
-        userId: req.user.clerkId,
-        userName: req.user.name,
-        userImage: req.user.profileImage,
+        userId: clerkId,
+        userName: name,
+        userImage: profileImage,
       },
       "token generated"
     )
--- backend/middlewares/protectRoute.middleware.js
+++ backend/middlewares/protectRoute.middleware.js
@@ -1,6 +1,7 @@
 import { requireAuth } from "@clerk/express";
 import { User } from "../models/User.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
 
 export const protectRoute = [
   requireAuth({signInUrl: "/sign-in"}),
