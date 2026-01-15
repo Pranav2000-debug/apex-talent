5e5cf26608df35ad9fb0e968cdbbf347d75b8359
Pranav2000-debug <pranav.ch410@gmail.com>
2026-01-15 18:49:28 +0530

    server side auth implemented

---
+++ backend/middlewares/protectRoute.middleware.js
@@ -0,0 +1,22 @@
+import { requireAuth } from "@clerk/express";
+import { User } from "../models/User.model.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+export const protectRoute = [
+  requireAuth({signInUrl: "/sign-in"}),
+  asyncHandler(async (req, res, next) => {
+    const clerkId = req.auth()?.userId;
+
+    if (!clerkId) {
+      throw new ApiError(401, "Unauthorized request");
+    }
+
+    const user = await User.findOne({ clerkId });
+    if (!user) {
+      throw new ApiError(404, "User not found");
+    }
+
+    req.user = user; // same as JWT flow
+    next();
+  }),
+];
